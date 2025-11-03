a3ca957cd5c52f038c377ee6e9ea70214eb320a8
catchJavass <juuipason25@gmail.com>
2025-11-03 14:35:41 +0900

    Inital commit

---
+++ src/main/java/com/webkit/travel_safety_backend/TravelSafetyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webkit.travel_safety_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSafetyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelSafetyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webkit/travel_safety_backend/TravelSafetyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webkit.travel_safety_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelSafetyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
