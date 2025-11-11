bff21a6fc487de3b017da5abe0d64098026d75ba
catchJavass <juuipason25@gmail.com>
2025-11-11 13:39:59 +0900

    dev

--- src/main/generated/com/webkit/travel_safety_backend/domain/model/mapper/UserMapperImpl.java
+++ src/main/generated/com/webkit/travel_safety_backend/domain/model/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-08T19:03:26+0900",
+    date = "2025-11-11T13:12:26+0900",
     comments = "version: 1.6.3, compiler: javac, environment: Java 21.0.2 (Oracle Corporation)"
 )
 @Component
